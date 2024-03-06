12c821c9c9f7abcc00bf42c2a85d8eaf93a46aac
chocochaos <git@chocochaos.com>
2024-03-06 10:56:36 +0100

    Test commit

--- Chatreader.lua
+++ Chatreader.lua
@@ -1,10 +1,3 @@
---This file is part of Game Master Genie.
---Copyright 2011-2014 Chocochaos
-
---Game Master Genie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
---Game Master Genie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
---You should have received a copy of the GNU General Public License along with Game Master Genie. If not, see <http://www.gnu.org/licenses/>.
-
 TicketTab = "General";
 
 -- 1d2h3m4s to number in seconds
