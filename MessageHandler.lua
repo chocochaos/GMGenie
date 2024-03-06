986d4f7b2ee0463147b300e220342d20512d1912
chocochaos <git@chocochaos.com>
2024-03-06 13:00:08 +0100

    Remove forgotten debug message

--- MessageHandler.lua
+++ MessageHandler.lua
@@ -3,7 +3,6 @@ function ChatFrame_MessageEventHandler(self, event, message, ...)
     local shouldMessageBeSuppressedInChat = false;
 
     if event == "CHAT_MSG_SYSTEM" then
-        GMGenie.showGMMessage('System message received');
         for _, messageHandler in pairs(GMGenie.CommandBus.responseHandlers) do
             shouldMessageBeSuppressedInChat = messageHandler(message);
 
